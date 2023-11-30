512fb82edc64263c6db55a943236577d57a40251
Julien BONZOM <bonzomjulien40@gmail.com>
2023-11-30 18:51:48 +0100

    ajout de l'exo moyenne

---
+++ Moyenne/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> tableau(0); //Un tableau de 0 entiers
+    int n(0);    //increment
+    int somme(0);
+    int moyenne(0);
+    int variable(0);
+
+    while(variable != -1)
+    {
+
+        cout << "Donnez la valeur d'entrée "<< n+1 <<"(tapez -1 pour sortir):" << endl;
+        cin >> variable;
+
+        if(variable==-1)
+        {
+            break;   // sortie si -1v est entré.
+        }
+        else
+        {
+            tableau.push_back(variable); //on ajoute une colonne avec la valeur variable
+
+        }
+
+     n++;
+    }
+    int const taille(tableau.size());
+    for(int i=0;i<taille;i++)
+    {
+    somme+=tableau[i];
+
+    }
+     moyenne = somme / taille;
+
+
+
+
+    cout << "La moyenne est egale à: " << moyenne << endl;   //affichage de la moyenne
+
+    return 0;
+}
