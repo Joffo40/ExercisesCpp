daa170e38378b7aaeeca497d33aaf758b306b023
Julien BONZOM <bonzomjulien40@gmail.com>
2024-04-22 18:16:59 +0200

    troisieme exercise exceptions

---
+++ exceptions/thirdExercise/thirdExercise/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        vector<int> a(10000000000000000000000000000000000000000000000000,1); //Un tableau bien trop grand
+    }
+    catch(exception const& e) //On rattrape les exceptions standard de tous types
+    {
+        cerr << "ERREUR : " << e.what() << endl; //On affiche la description de l'erreur
+    }
+    return 0;
+}
